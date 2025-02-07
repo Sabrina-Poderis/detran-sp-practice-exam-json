37d942845f11b30488e64ec159784ac1133e76d9
Zabrina <sabrina.poderis@gmail.com>
2025-02-07 17:01:08 -0300

    feat: insert rules in repository

--- backend/src/modules/QuestionDetran/repositories/QuestionsDetranRepository.ts
+++ backend/src/modules/QuestionDetran/repositories/QuestionsDetranRepository.ts
@@ -2,17 +2,24 @@ import QuestionDetranInterface from "@modules/QuestionDetran/ts/interfaces/Quest
 import QuestionTypeEnum from "@modules/QuestionDetran/ts/enums/QuestionTypeEnum";
 import QuestionDetranModel from "@modules/QuestionDetran/models/QuestionDetranModel";
 
-
 export default class QuestionsDetranRepository {
-  async findAll(): Promise<QuestionDetranInterface[]> {
-    return await QuestionDetranModel.find().exec();
+  async findAll(includeUnchecked: boolean = false): Promise<QuestionDetranInterface[]> {
+    const query: { answer: { $ne: string }, checked?: boolean} = { answer: { $ne: '?' } };
+    if (!includeUnchecked) {
+      query.checked = true;
+    }
+    return await QuestionDetranModel.find(query).exec();
   }
 
   async findById(id: number): Promise<QuestionDetranInterface | null> {
-    return await QuestionDetranModel.findOne({ id }).exec();
+    return await QuestionDetranModel.findOne({ id, answer: { $ne: '?' } }).exec();
   }
 
-  async findByType(type: QuestionTypeEnum): Promise<QuestionDetranInterface[]> {
-    return await QuestionDetranModel.find({ type }).exec();
+  async findByType(type: QuestionTypeEnum, includeUnchecked: boolean = false): Promise<QuestionDetranInterface[]> {
+    const query: { type: QuestionTypeEnum, answer: { $ne: string }, checked?: boolean } = { type, answer: { $ne: '?' } };
+    if (!includeUnchecked) {
+      query.checked = true;
+    }
+    return await QuestionDetranModel.find(query).exec();
   }
 }
